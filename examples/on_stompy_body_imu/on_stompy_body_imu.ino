be6758478896a4c8cc7c4e328525aabfa509a7cf
brett <brettgraham@gmail.com>
2018-07-03 13:28:16 -0400

    body firmware: added imu, thermocouple, rpm sensor

--- examples/on_stompy_body_imu/on_stompy_body_imu.ino
+++ examples/on_stompy_body_imu/on_stompy_body_imu.ino
@@ -1,5 +1,17 @@
-//#include "leg.h"
 #include <comando.h>
+
+// imu
+#include <NXPMotionSense.h>
+#include <Wire.h>
+#include <EEPROM.h>
+
+// thermocouple
+#include <Adafruit_MAX31856.h>
+#define TC_CS 10
+#define TC_DI 11
+#define TC_DO 12
+#define TC_CLK 13
+
 
 // 3000 / 1024.
 #define PSI_PER_TICK 2.9296875
@@ -15,14 +27,14 @@ TextProtocol text = TextProtocol(com);
 #define NAME 0  // imu
 
 #define CMD_NAME 0
-#define CMD_FEED_PRESSURE 1  // int, float
+#define CMD_FEED_PRESSURE 1  // analog: float
 
-#define CMD_FEED_OIL_TEMP 5  // serial?
-#define CMD_RETURN_OIL_TEMP 6  // serial?
+#define CMD_FEED_OIL_TEMP 5  // serial: float
+#define CMD_RETURN_OIL_TEMP 6  // serial: float
 
-#define CMD_HEADING 15  // imu, serial
+#define CMD_IMU_HEADING 15  // imu, serial: float, float, float
 
-#define CMD_ENGINE_RPM 21  // digital pulse per rev
+#define CMD_ENGINE_RPM 21  // digital pulse per rev: float
 // 2000 rpm, ~34 rev per sec, ~30 ms between pulses
 // setup an interrupt, count # of millis between
 // maybe average every 10 reads (iir)
@@ -136,6 +148,69 @@ void RPMBodySensor::report_sensor() {
   _cmd->finish_command();
 };
 
+
+class IMUBodySensor : public BodySensor {
+  public:
+    IMUBodySensor(CommandProtocol *cmd, byte index);
+    void report_sensor();
+    void update_filter();
+    void check();
+
+    NXPMotionSense imu;
+    NXPSensorFusion filter;
+};
+
+IMUBodySensor::IMUBodySensor(CommandProtocol *cmd, byte index):BodySensor(cmd, index) {
+  imu.begin();
+  filter.begin(100);
+};
+
+void IMUBodySensor::update_filter() {
+  if (imu.available()) {
+    float ax, ay, az;
+    float gx, gy, gz;
+    float mx, my, mz;
+
+    // Read the motion sensors
+    imu.readMotionSensor(ax, ay, az, gx, gy, gz, mx, my, mz);
+
+    // Update the SensorFusion filter
+    filter.update(gx, gy, gz, ax, ay, az, mx, my, mz);
+  };
+};
+
+void IMUBodySensor::check() {
+  update_filter();
+  this->BodySensor::check();
+};
+
+void IMUBodySensor::report_sensor() {
+  _cmd->start_command(_index);
+  _cmd->add_arg(filter.getRoll());
+  _cmd->add_arg(filter.getPitch());
+  _cmd->add_arg(filter.getYaw());
+  _cmd->finish_command();
+};
+
+class TemperatureBodySensor : public BodySensor {
+  public:
+    TemperatureBodySensor(CommandProtocol *cmd, byte index, byte cs_pin);
+    void report_sensor();
+
+    Adafruit_MAX31856 *tc;
+};
+
+TemperatureBodySensor::TemperatureBodySensor(CommandProtocol *cmd, byte index, byte cs_pin):BodySensor(cmd, index) {
+  tc = new Adafruit_MAX31856(cs_pin, TC_DI, TC_DO, TC_CLK);
+};
+
+
+void TemperatureBodySensor::report_sensor() {
+  _cmd->start_command(_index);
+  _cmd->add_arg(tc->readThermocoupleTemperature());
+  _cmd->finish_command();
+};
+
 // ------------------------------------------------
 
 
@@ -164,6 +239,18 @@ void rpm_tick() {
   rpm->tick();
 };
 
+IMUBodySensor *imu;
+
+void set_imu_period(CommandProtocol *cmd) {
+  imu->_set_period(cmd);
+};
+
+TemperatureBodySensor *feed_temp;
+
+void set_feed_temp_period(CommandProtocol *cmd) {
+  feed_temp->_set_period(cmd);
+};
+
 void setup(){
   Serial.begin(9600);
   //test = new AnalogBodySensor(&cmd, 1, 1);
@@ -171,16 +258,22 @@ void setup(){
   feed_pressure = new PressureBodySensor(
     &cmd, CMD_FEED_PRESSURE, FEED_PRESSURE_PIN);
   rpm = new RPMBodySensor(&cmd, CMD_ENGINE_RPM, ENGINE_RPM_PIN);
+  imu = new IMUBodySensor(&cmd, CMD_IMU_HEADING);
+  feed_temp = new TemperatureBodySensor(&cmd, CMD_FEED_OIL_TEMP, TC_CS);
 
   cmd.register_callback(feed_pressure->_index, set_feed_pressure_period);
   cmd.register_callback(rpm->_index, set_rpm_period);
+  cmd.register_callback(imu->_index, set_imu_period);
+  cmd.register_callback(feed_temp->_index, set_feed_temp_period);
 
   // setup rpm interrupt
-  attachInterrupt(ENGINE_RPM_PIN, rpm_tick, FALLING);
+  attachInterrupt(ENGINE_RPM_PIN, rpm_tick, RISING);
 }
 
 void loop() {
   //test->check();
   feed_pressure->check();
   rpm->check();
+  imu->check();
+  feed_temp->check();
 };
